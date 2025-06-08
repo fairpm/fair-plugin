2aac46d87f39e7d64cf2b0d836bb2382daaed635
Ramon Fincken <github-fair-pm@wpramon.com>
2025-06-08 18:26:04 +0200

    [DOCS] Extend comment to prevent fatal errors on generated strings
    Signed-off-by: Ramon Fincken <github-fair-pm@wpramon.com>

--- inc/salts/namespace.php
+++ inc/salts/namespace.php
@@ -8,7 +8,7 @@
 namespace FAIR\Salts;
 
 /**
- * This is the character set used for various functions.
+ * This is the character set used for various functions. Refrain using \ (backslash).
  */
 const CHARACTER_SET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_ []{}<>~`+=,.;:/?|';
 
