8bf8cff5e8372f89a6ef824ad92d8aa6107eece4
Andy Fragen <andy@thefragens.com>
2025-07-01 09:25:29 -0700

    Linting updates for #71 (#138)
    Signed-off-by: Andy Fragen <andy@thefragens.com>
    Co-authored-by: Colin Stewart <79332690+costdev@users.noreply.github.com>

--- inc/packages/class-releasedocument.php
+++ inc/packages/class-releasedocument.php
@@ -1,46 +1,75 @@
 <?php
+/**
+ * Create Release Document.
+ *
+ * @package FAIR
+ */
 
 namespace FAIR\Packages;
 
 use stdClass;
 use WP_Error;
 
+/**
+ * Class ReleaseDocument.
+ */
 class ReleaseDocument {
 	/**
+	 * Version.
+	 *
 	 * @var string
 	 */
 	public $version;
 
 	/**
+	 * Artifacts.
+	 *
 	 * @var stdClass
 	 */
 	public $artifacts;
 
 	/**
+	 * Provides.
+	 *
 	 * @var array
 	 */
 	public $provides;
 
 	/**
+	 * Requires.
+	 *
 	 * @var array
 	 */
 	public $requires;
 
 	/**
+	 * Suggests.
+	 *
 	 * @var array
 	 */
 	public $suggests;
 
 	/**
+	 * Authorization.
+	 *
 	 * @var array
 	 */
 	public $auth;
 
 	/**
+	 * Release.
+	 *
 	 * @var ReleaseDocument
 	 */
 	public $release;
 
+	/**
+	 * Collate data.
+	 *
+	 * @param  stdClass $data Data to parse.
+	 *
+	 * @return ReleaseDocument|WP_Error
+	 */
 	public static function from_data( stdClass $data ) {
 		$doc = new static();
 		$mandatory = [
--- inc/packages/did/class-document.php
+++ inc/packages/did/class-document.php
@@ -1,14 +1,47 @@
 <?php
+/**
+ * Create DID Document.
+ *
+ * @package FAIR
+ */
 
 namespace FAIR\Packages\DID;
 
 use stdClass;
 
+/**
+ * Class Document.
+ */
 class Document {
+	// phpcs:disable WordPress.NamingConventions.ValidVariableName
+	/**
+	 * DID.
+	 *
+	 * @var string
+	 */
 	public string $id;
+
+	/**
+	 * Service type.
+	 *
+	 * @var array
+	 */
 	public array $service;
+
+	/**
+	 * Verification method.
+	 *
+	 * @var array
+	 */
 	public array $verificationMethod;
 
+	/**
+	 * Constructor.
+	 *
+	 * @param  string $id DID.
+	 * @param  array  $service Service type.
+	 * @param  array $verificationMethod Verification method.
+	 */
 	public function __construct(
 		string $id,
 		array $service,
@@ -22,6 +55,7 @@ class Document {
 	/**
 	 * Get a service by type.
 	 *
+	 * @param string $type Service type.
 	 * @return stdClass Service data, including id and serviceEndpoint
 	 */
 	public function get_service( string $type ) : ?stdClass {
@@ -44,8 +78,9 @@ class Document {
 
 			$parsed = parse_url( $key->id );
 
-			// Only permit keys with IDs prefixed with 'fair'
+			// Only permit keys with IDs prefixed with 'fair'.
 			return str_starts_with( $parsed['fragment'], 'fair' );
 		} );
 	}
+	// phpcs:enable
 }
