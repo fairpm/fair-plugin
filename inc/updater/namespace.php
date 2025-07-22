6352d80351391fb4594dd8e3816e8baaab3ad413
Andy Fragen <andy@thefragens.com>
2025-07-22 08:13:53 -0700

    Elvis here too
    Signed-off-by: Andy Fragen <andy@thefragens.com>

--- inc/updater/namespace.php
+++ inc/updater/namespace.php
@@ -30,7 +30,7 @@ function bootstrap() {
  * @return void
  */
 function get_fair_release_data( $did ) : void {
- 	$releases = wp_cache_get( RELEASE_PACKAGES_CACHE_KEY ) ?: [];
+	$releases = wp_cache_get( RELEASE_PACKAGES_CACHE_KEY ) ?: [];
 
 	if ( $did ) {
 		$releases[ $did ] = get_latest_release_from_did( $did );
@@ -62,8 +62,7 @@ function upgrader_pre_download( $false ) : bool {
  * @return array
  */
 function maybe_add_accept_header( $args, $url ) : array {
-	$releases = wp_cache_get( RELEASE_PACKAGES_CACHE_KEY );
-	$releases = $releases ? $releases : [];
+	$releases = wp_cache_get( RELEASE_PACKAGES_CACHE_KEY ) ?: [];
 
 	if ( ! str_contains( $url, 'api.github.com' ) ) {
 		return $args;
