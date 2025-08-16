e8f58a6c482ad084bd6bd3442a19f42752c9bb75
Andy Fragen <andy@thefragens.com>
2025-08-16 09:04:05 -0700

    combine conditional checks
    Signed-off-by: Andy Fragen <andy@thefragens.com>

--- inc/packages/namespace.php
+++ inc/packages/namespace.php
@@ -176,20 +176,18 @@ function fetch_metadata_doc( string $url ) {
  * @return bool|array
  */
 function fetch_metadata_from_local( $response, $url ) {
-	if ( ! $response ) {
-		if ( str_contains( $url, home_url() ) ) {
-			$did = explode( '/', parse_url( $url, PHP_URL_PATH ) );
-			$did = array_pop( $did );
-			$body = wp_cache_get( 'fair-metadata-endpoint-' . $did, 'metadata-endpoints' );
-			$response = [];
-			$response = [
-				'headers' => [],
-				'body' => json_encode( $body ),
-			];
-			$response = ! $body ? false : $response;
-			if ( $response ) {
-				wp_cache_set( CACHE_KEY . md5( $url ), $response, 'metadata-docs', CACHE_LIFETIME );
-			}
+	if ( ! $response && str_contains( $url, home_url() ) ) {
+		$did = explode( '/', parse_url( $url, PHP_URL_PATH ) );
+		$did = array_pop( $did );
+		$body = wp_cache_get( 'fair-metadata-endpoint-' . $did, 'metadata-endpoints' );
+		$response = [];
+		$response = [
+			'headers' => [],
+			'body' => json_encode( $body ),
+		];
+		$response = ! $body ? false : $response;
+		if ( $response ) {
+			wp_cache_set( CACHE_KEY . md5( $url ), $response, 'metadata-docs', CACHE_LIFETIME );
 		}
 	}
 
