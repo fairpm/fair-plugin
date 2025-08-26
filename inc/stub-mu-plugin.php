acf46896f82ebc105f2673cc2ead970a0be2d6ad
Scott Kingsley Clark <scott@skc.dev>
2025-08-26 07:34:30 -0500

    Add an mu-plugin stub file with the same plugin headers
    This is the flip side of https://github.com/fairpm/fair-plugin/pull/234 which instead of updating the plugin file to support being used as the mu-plugin file itself, this will be a separate stub file that could then be copied/used by hosting environments as the mu-plugin loader and includes plugin header information.
    
    Signed-off-by: Scott Kingsley Clark <scott@skc.dev>

---
+++ inc/stub-mu-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: FAIR - Federated and Independent Repositories
+ * Description: Make your site more FAIR.
+ * Version: 0.4.1
+ * Author: FAIR Contributors
+ * License: GPLv2
+ * Requires at least: 5.4
+ * Requires PHP: 7.4
+ * Text Domain: fair
+ * Domain Path: /languages
+ * Update URI: https://api.fair.pm
+ * GitHub Plugin URI: https://github.com/fairpm/fair-plugin
+ * Primary Branch: main
+ * Release Asset: true
+ * Network: true
+ */
+
+require_once __DIR__ . '/fair-plugin/plugin.php';
