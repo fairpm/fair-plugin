eb1c0573890b7517d902fe60be67803593a27d19
costdev <79332690+costdev@users.noreply.github.com>
2025-09-12 05:14:00 +0100

    Revert "Only include composer files for distribution."
    This reverts commit ca4c5eab379a9a901e57dbe4f58cc10a2bcc6517.
    
    Signed-off-by: costdev <79332690+costdev@users.noreply.github.com>

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit27c052e9ce72ede10097105f42b32ad5
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit27c052e9ce72ede10097105f42b32ad5::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
