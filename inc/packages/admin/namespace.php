6e4e82efb7563d2a842b1583d621f64eb774f20d
Andy Fragen <andy@thefragens.com>
2025-10-05 15:03:00 -0700

    revert
    Signed-off-by: Andy Fragen <andy@thefragens.com>

--- inc/packages/admin/namespace.php
+++ inc/packages/admin/namespace.php
@@ -392,11 +392,6 @@ function alter_slugs( $res, $action, $args ) {
 			continue;
 		}
 
-		$plugins = get_plugins();
-		if ( array_key_exists( $plugin['file'], $plugins ) ) {
-			continue;
-		}
-
 		$did = $plugin['_fair']['id'];
 		$plugin['slug'] = esc_attr( $plugin['slug'] . '-' . str_replace( ':', '--', $did ) );
 	}
