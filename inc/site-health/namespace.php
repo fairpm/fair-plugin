498421e81cf40e87dc947dc108dd8244ab8d2816
Joe Dolson <design@joedolson.com>
2025-11-23 16:49:55 -0600

    Address PHPCS
    Adds a PHPCS exception because this string actually needs to come from WordPress core.
    
    Signed-off-by: Joe Dolson <design@joedolson.com>

--- inc/site-health/namespace.php
+++ inc/site-health/namespace.php
@@ -14,6 +14,11 @@ function bootstrap() {
 	add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\enqueue_media_scripts' );
 }
 
+/**
+ * Enqueue scripts for Site Health.
+ *
+ * @param string $hook_suffix Hook to identify current screen.
+ */
 function enqueue_media_scripts( $hook_suffix ) {
 
 	if ( 'site-health.php' !== $hook_suffix ) {
@@ -31,12 +36,18 @@ function enqueue_media_scripts( $hook_suffix ) {
 
 }
 
+/**
+ * Set up regular expression used for handling error messages.
+ *
+ * @return string
+ */
 function build_error_message_regex() {
 	$regex = str_replace(
 		[ '%1\$s', '%2\$s' ],
-		[ '(?:.*?)', '(.*)'],
-		preg_quote( __( 'Your site is unable to reach WordPress.org at %1$s, and returned the error: %2$s' ) )
+		[ '(?:.*?)', '(.*)' ],
+		preg_quote( __( 'Your site is unable to reach WordPress.org at %1$s, and returned the error: %2$s' ) ) // phpcs:ignore WordPress.WP.I18n.MissingArgDomain
 	);
 	$regex = $regex . '<\/p>';
+
 	return $regex;
 }
