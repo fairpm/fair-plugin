b9e4c8733de294c710a53e013ee78892608ed931
Andy Fragen <andy@thefragens.com>
2025-12-01 07:52:16 -0800

    Rename `alter_slugs()`
    Signed-off-by: Andy Fragen <andy@thefragens.com>

--- inc/packages/admin/namespace.php
+++ inc/packages/admin/namespace.php
@@ -34,7 +34,7 @@ function bootstrap() {
 	add_action( 'install_plugins_' . TAB_DIRECT, __NAMESPACE__ . '\\render_tab_direct' );
 	add_action( 'load-plugin-install.php', __NAMESPACE__ . '\\load_plugin_install' );
 	add_action( 'install_plugins_pre_plugin-information', __NAMESPACE__ . '\\maybe_hijack_plugin_info', 0 );
-	add_filter( 'plugins_api_result', __NAMESPACE__ . '\\alter_slugs', 10, 3 );
+	add_filter( 'plugins_api_result', __NAMESPACE__ . '\\handle_did_in_search_results', 10, 3 );
 	add_filter( 'plugins_api_result', __NAMESPACE__ . '\\sort_sections_in_api', 15, 1 );
 	add_filter( 'plugin_install_action_links', __NAMESPACE__ . '\\maybe_hijack_plugin_install_button', 10, 2 );
 	add_filter( 'plugin_install_description', __NAMESPACE__ . '\\maybe_add_data_to_description', 10, 2 );
@@ -408,7 +408,7 @@ function maybe_hijack_legacy_plugin_info() {
  * @param object          $args   Plugin API arguments.
  * @return object|WP_Error
  */
-function alter_slugs( $res, $action, $args ) {
+function handle_did_in_search_results( $res, $action, $args ) {
 	if ( 'query_plugins' !== $action ) {
 		return $res;
 	}
@@ -462,7 +462,7 @@ function sort_sections_in_api( $res ) {
 /**
  * Override the install button, for bridged plugins.
  *
- * Bridged plugins appear in the API (via `alter_slugs()`) with slugs like
+ * Bridged plugins appear in the API (via `handle_did_in_search_results()`) with slugs like
  * `plugin-name-did--method--msid`, however they are installed to
  * `plugin-name-didhash`. In order to show the correct button, we need to check
  * against the install slug, not the API slug.
