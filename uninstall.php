bf8c6c3e6ac467b7f8a4026764e6456b6e843c07
Norcross <andrew.norcross@gmail.com>
2025-12-01 13:39:33 -0500

    replacing string with namespaced constant
    Signed-off-by: Norcross <andrew.norcross@gmail.com>

--- uninstall.php
+++ uninstall.php
@@ -10,8 +10,15 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
 
+// Include the avatar namespace so we can use the constant.
+require_once __DIR__ . '/inc/avatars/namespace.php';
+
 // Delete our single keys.
 delete_option( 'fair_indexnow_key' );
 
 // Our multisite level options.
-delete_site_option( 'fair_avatar_source' );
+delete_site_option( \FAIR\Avatars\AVATAR_SRC_SETTING_KEY );
+
+// Any transients we may have.
+delete_site_transient( 'update_plugins' );
+delete_site_transient( 'update_themes' );
