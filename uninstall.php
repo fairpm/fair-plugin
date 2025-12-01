8e2ae512a1c701cde8b54b4c007f54986cc2623b
Norcross <andrew.norcross@gmail.com>
2025-12-01 16:38:23 -0500

    deleting FAIR user meta
    Signed-off-by: Norcross <andrew.norcross@gmail.com>

--- uninstall.php
+++ uninstall.php
@@ -13,12 +13,24 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 // Include the avatar namespace so we can use the constant.
 require_once __DIR__ . '/inc/avatars/namespace.php';
 
-// Delete our single keys.
+// Delete any single or multisite keys.
 delete_option( 'fair_indexnow_key' );
-
-// Our multisite level options.
 delete_site_option( \FAIR\Avatars\AVATAR_SRC_SETTING_KEY );
 
-// Any transients we may have.
+// Delete any transients we may have.
 delete_site_transient( 'update_plugins' );
 delete_site_transient( 'update_themes' );
+
+global $wpdb;
+
+// Prepare our delete query.
+$query_args = $wpdb->prepare("
+	DELETE FROM $wpdb->usermeta
+	WHERE       meta_key IN (
+		'%s',
+		'%s'
+	)
+", esc_attr( 'fair_avatar_site_id' ), esc_attr( 'fair_avatar_id' ) );
+
+// And actually run it.
+$wpdb->query( $query_args );
