dc8c4eaad7e7a6039de62d0b656f7e19b8dc83df
Andy Fragen <andy@thefragens.com>
2025-12-13 16:41:28 -0800

    move to more logical place in file
    Signed-off-by: Andy Fragen <andy@thefragens.com>

--- inc/packages/namespace.php
+++ inc/packages/namespace.php
@@ -86,6 +86,55 @@ function get_did_hash( string $id ) {
 }
 
 /**
+ * Get a package's DID by its path.
+ *
+ * @param string $path The absolute path to the package's directory or main file.
+ * @param string $type The type of package. Allowed types are 'plugin' or 'theme'.
+ * @return DID|WP_Error The DID object on success, WP_Error on failure.
+ */
+function get_did_by_path( $path, $type ) {
+	global $wp_filesystem;
+
+	if ( $type === 'theme' ) {
+		if ( ! str_ends_with( $path, 'style.css' ) ) {
+			$path = trailingslashit( $path ) . 'style.css';
+		}
+
+		$id = get_file_data( $path, [ 'id' => 'Theme ID' ] )['id'];
+		if ( $id ) {
+			return parse_did( $id );
+		}
+	}
+
+	if ( $type === 'plugin' ) {
+		if ( str_ends_with( $path, '.php' ) ) {
+			$id = get_file_data( $path, [ 'id' => 'Plugin ID' ] )['id'];
+			return parse_did( $id );
+		}
+
+		$files = $wp_filesystem->dirlist( $path ) ?: false;
+		if ( ! $files ) {
+			// Finding a DID is impossible.
+			return new WP_Error( 'fair.packages.dirlist_failed', __( "The package's file list could not be retrieved.", 'fair' ) );
+		}
+
+		foreach ( $files as $filename => $data ) {
+			if ( $data['type'] !== 'f' || ! str_ends_with( $filename, '.php' ) ) {
+				continue;
+			}
+
+			$filepath = trailingslashit( $path ) . $filename;
+			$id = get_file_data( $filepath, [ 'id' => 'Plugin ID' ] )['id'];
+			if ( $id ) {
+				return parse_did( $id );
+			}
+		}
+	}
+
+	return new WP_Error( 'fair.packages.none_found', __( 'No FAIR packages were found.', 'fair' ) );
+}
+
+/**
  * Get DID document.
  *
  * @param string $id DID.
@@ -995,52 +1044,4 @@ function get_plugin_information( $result, $action, $args ) {
 	return (object) $api_data;
 }
 
-/**
- * Get a package's DID by its path.
- *
- * @param string $path The absolute path to the package's directory or main file.
- * @param string $type The type of package. Allowed types are 'plugin' or 'theme'.
- * @return DID|WP_Error The DID object on success, WP_Error on failure.
- */
-function get_did_by_path( $path, $type ) {
-	global $wp_filesystem;
-
-	if ( $type === 'theme' ) {
-		if ( ! str_ends_with( $path, 'style.css' ) ) {
-			$path = trailingslashit( $path ) . 'style.css';
-		}
-
-		$id = get_file_data( $path, [ 'id' => 'Theme ID' ] )['id'];
-		if ( $id ) {
-			return parse_did( $id );
-		}
-	}
-
-	if ( $type === 'plugin' ) {
-		if ( str_ends_with( $path, '.php' ) ) {
-			$id = get_file_data( $path, [ 'id' => 'Plugin ID' ] )['id'];
-			return parse_did( $id );
-		}
-
-		$files = $wp_filesystem->dirlist( $path ) ?: false;
-		if ( ! $files ) {
-			// Finding a DID is impossible.
-			return new WP_Error( 'fair.packages.dirlist_failed', __( "The package's file list could not be retrieved.", 'fair' ) );
-		}
-
-		foreach ( $files as $filename => $data ) {
-			if ( $data['type'] !== 'f' || ! str_ends_with( $filename, '.php' ) ) {
-				continue;
-			}
-
-			$filepath = trailingslashit( $path ) . $filename;
-			$id = get_file_data( $filepath, [ 'id' => 'Plugin ID' ] )['id'];
-			if ( $id ) {
-				return parse_did( $id );
-			}
-		}
-	}
-
-	return new WP_Error( 'fair.packages.none_found', __( 'No FAIR packages were found.', 'fair' ) );
-}
 // phpcs:enable
