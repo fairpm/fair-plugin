82d05c25e0d567c588533f7d1ac41e0558d8815e
Colin Stewart <79332690+costdev@users.noreply.github.com>
2025-12-24 01:18:24 +0000

    Pass 4 arguments to `maybe_rename_on_package_download`. (#394)

--- inc/packages/namespace.php
+++ inc/packages/namespace.php
@@ -710,7 +710,7 @@ function get_package_data( $did ) {
  */
 function upgrader_pre_download( $false ) : bool {
 	add_filter( 'http_request_args', 'FAIR\\Packages\\maybe_add_accept_header', 20, 2 );
-	add_filter( 'upgrader_source_selection', __NAMESPACE__ . '\\maybe_rename_on_package_download', 11, 3 );
+	add_filter( 'upgrader_source_selection', __NAMESPACE__ . '\\maybe_rename_on_package_download', 11, 4 );
 	return $false;
 }
 
