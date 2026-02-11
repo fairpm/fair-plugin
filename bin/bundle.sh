c5b3ff51137e71f3aa60f3fb7519cf2d0c7164b7
Andy Fragen <andy@thefragens.com>
2026-02-11 11:30:04 -0800

    update validation check
    Signed-off-by: Andy Fragen <andy@thefragens.com>

--- bin/bundle.sh
+++ bin/bundle.sh
@@ -55,7 +55,7 @@ for VERSION in $AVAILABLE_VERSIONS; do
 	EXPECTED_HASH=$(curl -sSL "$WP_ZIP_URL.sha1")
 
 	# Skip if we can't get a valid hash.
-	if ![[ $EXPECTED_HASH =~ ^[a-z0-9]{40}$ ]]; then
+	if [[ ! "$EXPECTED_HASH" =~ ^[a-z0-9]{40}$ ]]; then
 		echo "Failed to fetch valid hash for $VERSION" >&2
 		continue
 	fi
