c569ddf212bf4df12268d34014985cbf9f83f5e7
Ipstenu (Mika Epstein) <Ipstenu@users.noreply.github.com>
2026-07-14 07:33:18 -0700

    Release 1.4.1 (#515)
    Signed-off-by: Kaspars Dambis <hi@kaspars.net>
    Signed-off-by: Chuck Adams <chaz@chaz.works>
    Signed-off-by: Ipstenu (Mika Epstein) <Ipstenu@users.noreply.github.com>
    Signed-off-by: Carrie Dils <cdils@users.noreply.github.com>
    Signed-off-by: cdils <cdils@users.noreply.github.com>
    Co-authored-by: Kaspars Dambis <hi@kaspars.net>
    Co-authored-by: Namith Jawahar <48271037+namithj@users.noreply.github.com>
    Co-authored-by: Andy Fragen <andy@thefragens.com>
    Co-authored-by: Chuck Adams <chaz@chaz.works>
    Co-authored-by: Colin Stewart <79332690+costdev@users.noreply.github.com>
    Co-authored-by: Carrie Dils <cdils@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- inc/packages/admin/namespace.php
+++ inc/packages/admin/namespace.php
@@ -29,7 +29,7 @@ function bootstrap() {
 	add_filter( 'plugins_api', __NAMESPACE__ . '\\handle_did_during_ajax', 10, 3 );
 	add_filter( 'plugins_api', 'FAIR\\Packages\\search_by_did', 10, 3 );
 	add_filter( 'upgrader_package_options', 'FAIR\\Packages\\cache_did_for_install', 10, 1 );
-	add_action( 'upgrader_post_install', 'FAIR\\Packages\\delete_cached_did_for_install', 10, 3 );
+	add_action( 'upgrader_post_install', 'FAIR\\Packages\\delete_cached_did_for_install', 10 );
 	add_filter( 'upgrader_pre_download', 'FAIR\\Packages\\upgrader_pre_download', 10, 1 );
 	add_action( 'install_plugins_' . TAB_DIRECT, __NAMESPACE__ . '\\render_tab_direct' );
 	add_action( 'load-plugin-install.php', __NAMESPACE__ . '\\load_plugin_install' );
@@ -44,7 +44,9 @@ function bootstrap() {
 	// Needed for pre WordPress 6.9 compatibility.
 	if ( ! is_wp_version_compatible( '6.9' ) ) {
 		add_action( 'install_plugins_featured', __NAMESPACE__ . '\\replace_featured_message' );
-		add_action( 'admin_init', fn() => remove_action( 'install_plugins_featured', 'install_dashboard' ) );
+		add_action( 'admin_init', function () {
+			remove_action( 'install_plugins_featured', 'install_dashboard' );
+		} );
 	}
 }
 
@@ -123,8 +125,6 @@ function handle_did_during_ajax( $result, $action, $args ) {
 	if ( ! preg_match( '/^did:plc:.+$/', $did ) ) {
 		return $result;
 	}
-
-	( new Updater\Updater( $did ) )->run();
 
 	Packages\add_package_to_release_cache( $did );
 	add_filter( 'http_request_args', 'FAIR\\Packages\\maybe_add_accept_header', 20, 2 );
@@ -499,7 +499,7 @@ function maybe_hijack_plugin_install_button( $links, $plugin ) {
 		$requires_wp  = isset( $plugin['requires'] ) ? $plugin['requires'] : null;
 		$compatible_php = is_php_version_compatible( $requires_php );
 		$compatible_wp  = is_wp_version_compatible( $requires_wp );
-		$name = strip_tags( $plugin['name'] . ' ' . $plugin['version'] );
+		$name = wp_strip_all_tags( $plugin['name'] . ' ' . $plugin['version'] );
 		$link = wp_get_plugin_action_button( $name, $plugin_override, $compatible_php, $compatible_wp );
 	}
 	return $links;
